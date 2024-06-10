72c037787d8908dae4efa2af0249533cd49f021b
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2024-06-10 23:47:50 +0600

    update icon, appBar, floatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -11,18 +11,50 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
 
-    return const MaterialApp(home: HomeActivity());
+    return MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.red),
+        darkTheme: ThemeData(primarySwatch: Colors.indigo),
+        color: Colors.blue,
+        debugShowCheckedModeBanner: false,
+        home: HomeActivity());
   }
 
 }
 class HomeActivity extends StatelessWidget{
   const HomeActivity({super.key});
 
+  MySnackBar(message, context){
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message))
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter App"),),
-      body: Text("Hello Flutter!!!"),
+      appBar: AppBar(
+        title: Text("Flutter App"),
+        titleSpacing: 10,
+        centerTitle: false,
+        toolbarHeight: 50,
+        toolbarOpacity: 0.9,
+        elevation: 50,
+        backgroundColor: Colors.red,
+        actions: [
+          IconButton(onPressed: (){MySnackBar("Comments", context);}, icon: Icon(Icons.comment)),
+          IconButton(onPressed: (){MySnackBar("search", context);}, icon: Icon(Icons.search)),
+          IconButton(onPressed: (){MySnackBar("Account", context);}, icon: Icon(Icons.account_circle_outlined)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        elevation: 10,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+        onPressed: (){
+          MySnackBar("I'm floating action button", context);
+        },
+      ),
+      body: Text("Hello Flutter!!!")
     );
   }
   
