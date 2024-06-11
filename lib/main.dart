fc284af6a48a97361f4f0a60bbc4d15f8a84e7d2
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2024-06-11 10:24:04 +0600

    footer and left drawer

--- lib/main.dart
+++ lib/main.dart
@@ -54,6 +54,45 @@ class HomeActivity extends StatelessWidget{
           MySnackBar("I'm floating action button", context);
         },
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 2,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home),label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.contact_phone),label: "contact"),
+          BottomNavigationBarItem(icon: Icon(Icons.person_2),label: "profile"),
+        ],
+        onTap: (int index){
+          if(index == 0){
+            MySnackBar("Home Menu", context);
+          }
+          if(index == 1) {
+            MySnackBar("contact Menu", context);
+          }
+          if(index == 2){
+            MySnackBar("profile Menu", context);
+          }
+        },
+
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.all(0),
+                child: UserAccountsDrawerHeader(
+                  decoration: BoxDecoration(color: Colors.black54),
+                  accountName: Text("Hasib"),
+                  accountEmail: Text("hasib@gmail.com"),
+                  currentAccountPicture: Image.network("https://media.licdn.com/dms/image/C5603AQH5w_IAWgu_-g/profile-displayphoto-shrink_800_800/0/1595098814626?e=1723680000&v=beta&t=Eb-4iG2egaI6Uvl5GMzhzWkAgrXTcEkrC52Yzq5GXSY"),
+                  onDetailsPressed: (){MySnackBar("This is profile", context);},
+            )),
+            ListTile(title: Text("Home"),leading: Icon(Icons.home), onTap: (){MySnackBar("home", context);}),
+            ListTile(title: Text("Contact"),leading: Icon(Icons.contact_phone), onTap: (){MySnackBar("contact", context);}),
+            ListTile(title: Text("Email"),leading: Icon(Icons.email), onTap: (){MySnackBar("email", context);}),
+            ListTile(title: Text("Profile"),leading: Icon(Icons.person_2_sharp), onTap: (){MySnackBar("profile", context);})
+          ],
+        ),
+      ),
       body: Text("Hello Flutter!!!")
     );
   }
